d0812332a3ba98bced1f98829c8226434055156b
Simon Streit <streit.simon5@gmail.com>
2021-09-16 16:11:17 +0200

    update dspace

--- images/configs/startup.sh
+++ images/configs/startup.sh
@@ -2,6 +2,5 @@
 
 bash /usr/local/tomcat/bin/startup.sh
 /dspace/bin/dspace dsrun org.dspace.app.util.InitializeEntities -f /dspace/config/relationship-types.xml
-bash /usr/local/tomcat/bin/shutdown.sh
-bash /usr/local/tomcat/bin/startup.sh
-tail -f logs/catalina.out
+catalina.sh stop 30
+catalina.sh run
